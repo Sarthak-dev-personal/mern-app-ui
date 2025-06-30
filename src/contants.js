dead818b7b76713062c5c9c5c9889d0c496448f2
Sarthak Kapoor <sarthakkapoor@Sarthaks-MacBook-Air.local>
2025-06-30 21:25:57 +0530

    Changing the base url to cater to server server url

--- src/contants.js
+++ src/contants.js
@@ -1,1 +1,9 @@
-export const BASE_URL = "http://localhost:3000";
+/**
+ * Commenting out the below to use the actual url with EC2 instance public IP.
+ */
+//export const BASE_URL = "http://localhost:3000";
+/**
+ * The config that we have set for nginx will automatically convert the api url to something like
+ * http://13.60.221.171/feed or http://13.60.221.171/login etc.
+ */
+export const BASE_URL = "/api";
